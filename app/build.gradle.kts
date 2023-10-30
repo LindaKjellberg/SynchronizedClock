b27ffa72c1ba0c97390154de55758becee157a1e
Linda Kjellberg <lindakjellberg7@gmail.com>
2023-10-30 21:07:24 +0100

    Add Apache Commons Net Library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 
 dependencies {
 
+    implementation("commons-net:commons-net:3.6")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
