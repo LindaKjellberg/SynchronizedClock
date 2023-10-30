05bd12b98c162852003d9099452250f2f8b08f04
Linda Kjellberg <lindakjellberg7@gmail.com>
2023-10-30 17:06:27 +0100

    Add findById for serverTime

--- app/src/main/java/com/example/synchronizedclock/MainActivity.java
+++ app/src/main/java/com/example/synchronizedclock/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+//import com.example.synchronizedclock.client.ServerTime;
+
 
 public class MainActivity extends AppCompatActivity {
-    // onCreate.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //@Composable
+        //onCreate
         //communicate with text fields
-      /*  public void  (View view){
-            View view = findViewById(R.id.systemTime);
-        }*/
+        final TextView serverTime = (TextView) findViewById(R.id.serverTime);
+        serverTime.setText("Display serverTime");
+
     }
 }
