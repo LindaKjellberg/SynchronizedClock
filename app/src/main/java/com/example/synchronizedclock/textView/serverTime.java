c2eb351581b4b283aae6bd4349c1c2de85ae720d
Linda Kjellberg <lindakjellberg7@gmail.com>
2023-10-31 16:01:11 +0100

    Add package textView and class serverTime

---
+++ app/src/main/java/com/example/synchronizedclock/textView/serverTime.java
@@ -0,0 +1,17 @@
+/*package com.example.synchronizedclock.textView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.synchronizedclock.R;
+
+public class serverTime extends AppCompatActivity {
+
+    //Following lines updates the text field elements in UI using their IDs:
+    //serverTime
+    final TextView serverTime = (TextView) findViewById(R.id.serverTime);
+        serverTime.setText(remoteNTPTime.toDateString());
+}
+*/
